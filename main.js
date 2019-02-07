fa26c37f12fc7466ba1660d9dd7270224062f23a
welsea <welsea.42@gmail.com>
2019-02-07 15:34:50 +0800

    svg

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ function createWindow() {
     
     // 创建浏览器窗口
     win = new BrowserWindow({
-        width: 800,
-        height: 600
+      width: 980,
+      minWidth: 680,
+      height: 800,
     })
 
     // 然后加载应用的 index.html。
@@ -15,6 +16,7 @@ function createWindow() {
 
     // 打开开发者工具
     win.webContents.openDevTools()
+    
     win.on('closed', () => {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
@@ -28,6 +30,7 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // 当全部窗口关闭时退出。
+//macOS
 app.on('window-all-closed', () => {
   // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
   // 否则绝大部分应用及其菜单栏会保持激活。
