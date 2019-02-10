01aa6de6c28b5c8b8d4aef3eae9352f86dace47f
welsea <welsea.42@gmail.com>
2019-02-10 22:41:06 +0800

    导航

--- js/nav.js
+++ js/nav.js
@@ -4,11 +4,36 @@ $(document).on('click','.nav-icon',function(){
     // console.log(id)
     $('.nav2').each(function(index,element){
         if(($(element).data('id'))==id){
-            $(element).toggleClass('hidden')
-            // console.log(id)
+            $(element).toggleClass('hidden');
         }else{
             if(!($(element).hasClass('hidden')))
                 $(element).addClass('hidden')
         }
     });
 })
+
+//nav点击变色
+$(document).on('click','.nav-icon',function(){
+    $(this).toggleClass('nav-icon-click');
+    if($(this).parents().hasClass('nav-btm')){
+        $(this).parents('.nav-btm').siblings('.nav1').children('.nav-icon').removeClass('nav-icon-click');
+        $(this).parent().siblings('.nav1').children('.nav-icon').removeClass('nav-icon-click')
+    }else{
+       $(this).parent().siblings('.nav1').children('.nav-icon').removeClass('nav-icon-click');
+       $('.nav-btm').find('.nav1 .nav-icon').removeClass('nav-icon-click')
+    }
+})
+//nav2
+$(document).on('click','.item',function(){
+    $('.item').each(function(index,element){
+        $(element).removeClass('item-click')
+    })
+    $(this).toggleClass('item-click');
+})
+
+//展开列表
+$(document).on('click','.drop-title',function(){
+    $(this).find('.icon').toggleClass('icon-unfold');
+    $(this).next('.drop-items').toggleClass('hidden')
+})
+
